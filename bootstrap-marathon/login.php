a42f7c4c78e7be0805543375b05c21ec73271d20
Tomas M <31823865+towmass@users.noreply.github.com>
2018-04-13 11:56:08 +0100

    update

--- bootstrap-marathon/login.php
+++ bootstrap-marathon/login.php
@@ -15,18 +15,21 @@
     <?php
         include 'navbar.php'
     ?>
+    <br>
 
-<form>
-  <div class="form-group">
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
+    <div class="container">
+        <form>
+        <div class="form-group">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-success">Submit</button>
+        </form>
+    </div>
+    
   </body>
 
 </html>
